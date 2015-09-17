b59b338f2aa3fc9252de574e8cece62242842a12
Robert Kirberich <robert@kirberich.de>
2015-09-17 17:36:24 +0100

    Require login for main page

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import User
 from temperature.models import TemperatureController
 
+
+@login_required
 def home(request):
     users = User.objects.all()
     controllers = [c.get_child() for c in TemperatureController.objects.all()]
