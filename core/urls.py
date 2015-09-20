9d1f2966594301db1eb3b790d5af2cbeb92bfa00
Robert Kirberich <robert@kirberich.de>
2015-09-20 12:34:32 +0100

    Add presence view

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^$', 'core.views.home', name='home'),
+    url(r'^$', 'core.views.main', name='main'),
+
+    url(r'^api/presence/(?P<user_id>.*)/$', 'core.views.api_presence', name='api_presence'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,13 +1,41 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+import json
 
-from .models import User
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.decorators import login_required
+from django.http.response import JsonResponse
+from django.shortcuts import render
+from django.utils.timezone import now
+from django.views.decorators.csrf import csrf_exempt
+
+
+from .models import User, Device
 from temperature.models import TemperatureController
 
 
 @login_required
-def home(request):
+def main(request):
     users = User.objects.all()
     controllers = [c.get_child() for c in TemperatureController.objects.all()]
     # [c.update() for c in controllers]
     return render(request, "main.html", {'users': users, 'controllers': controllers})
+
+
+@csrf_exempt
+def api_presence(request, user_id):
+    user = User.objects.get(id=user_id)
+
+    data = json.loads(request.body)
+
+    if user.api_key != data.get('api_key'):
+        raise PermissionDenied('invalid api key')
+
+    device = Device.objects.get(id=data['device_id'])
+    device.is_present = data['is_present']
+    device.last_seen = now()
+    device.save()
+
+    response_data = {
+        'is_present': device.is_present
+    }
+
+    return JsonResponse(response_data)
